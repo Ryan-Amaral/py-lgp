39578b76dc8824ab4512afbab5b9b65ff82d9400
Ryan Amaral <ryan_amaral@live.com>
2019-05-10 14:45:17 -0300

    initial implement trainer

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # py-lgp
 Linear Genetic Programming algorithm written in Python.
 
-Program functionality is now fully in place, now just need to add training
-related features to the project.
+Still WIP to make sure components are working.
--- lgp/trainer.py
+++ lgp/trainer.py
@@ -6,15 +6,15 @@ Creates and maintains a population of programs.
 """
 class Trainer:
 
-    def __init__(self, popSize=200, gap=0.5, maxProgSize=128,
-            numOutRegs=8, numMemRegs=16, numFgtRegs=8,
+    def __init__(self, numActions, popSize=200, gap=0.5, maxProgSize=128,
+            numMemRegs=16, numFgtRegs=8,
             pInstAdd=1, pInstDel=1, pInstSwp=1, pInstMut=1, pProgMut=1):
 
         self.popSize = popSize
         self.gap = gap
 
         Program.maxProgSize = maxProgSize
-        Program.numOutRegs = numOutRegs
+        Program.numOutRegs = numActions
         Program.numMemRegs = numMemRegs
         Program.numFgtRegs = numFgtRegs
         Program.pInstAdd = pInstAdd
@@ -42,16 +42,29 @@ class Trainer:
         else: # sort based on fitnesses
             if isinstance(tasks, str): # single task
                 return sorted(self.programs,
-                        key=lambda prg: prg.outcomes.get(tasks, None))
+                        key=lambda prg: prg.outcomes.get(tasks, None),
+                        reverse=True)
             else: # multi task
                 pass # implement later when needed
 
-    def evolve(self, tasks, fitType='min', ):
-        self.select()
+    def evolve(self, tasks, fitType='min'):
+        self.select(tasks, fitType)
         self.generate()
+        self.curGen += 1
 
-    def select(self):
-        pass
+    def select(self, tasks, fitType):
+        numKeep = self.popSize - int(self.popSize * self.gap) # agents to keep
+        if isinstance(tasks, str): # single task
+            self.programs = sorted(self.programs,
+                    key=lambda prg: prg.outcomes.get(tasks, None),
+                    reverse=True)[:numKeep]
+        else: # multi task
+            pass
 
     def generate(self):
-        pass
+        # generate this many new ones
+        for i in range(self.popSize - len(self.programs)):
+            # for now just create randoms for testing
+            self.programs.append(
+                    Program(progSize=random.randint(1,self.maxProgSize),
+                            getCreate=self.curGen))
