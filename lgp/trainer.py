149f31012e6c3569983be5a769e35bc396431a95
Ryan Amaral <ryan_amaral@live.com>
2019-05-10 11:28:45 -0300

    trainer init header

---
+++ lgp/trainer.py
@@ -0,0 +1,9 @@
+"""
+Creates and maintains a population of programs.
+"""
+class Trainer:
+
+    def __init__(self, popSize=200, maxProgSize=128,
+            numOutRegs=8, numMemRegs=16, numFgtRegs=8,
+            pInstAdd=1, pInstDel=1, pInstSwp=1, pInstMut=1, pProgMut=1):
+        pass
