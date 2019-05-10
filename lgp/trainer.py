1227a46e21eed798085c947b8a075a9b88b1441f
Ryan Amaral <ryan_amaral@live.com>
2019-05-10 13:24:46 -0300

    trainer function headers

--- lgp/trainer.py
+++ lgp/trainer.py
@@ -3,7 +3,25 @@ Creates and maintains a population of programs.
 """
 class Trainer:
 
-    def __init__(self, popSize=200, maxProgSize=128,
+    def __init__(self, popSize=200, gap=0.5, maxProgSize=128,
             numOutRegs=8, numMemRegs=16, numFgtRegs=8,
             pInstAdd=1, pInstDel=1, pInstSwp=1, pInstMut=1, pProgMut=1):
+        pass
+
+    def getAllAgents(self):
+        pass
+
+    def getBestAgents(self):
+        pass
+
+    def initPop(self):
+        pass
+
+    def evolve(self):
+        pass
+
+    def select(self):
+        pass
+
+    def generate(self):
         pass
